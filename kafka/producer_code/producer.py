7d24a6b71978b18a1f60cd4b8df9f591d999c4e8
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-08-03 09:55:10 +0900

    Update producer.py

--- kafka/producer_code/producer.py
+++ kafka/producer_code/producer.py
@@ -2,12 +2,14 @@ from kafka import KafkaProducer
 import generator_log
 import time
 
-broker = ['broker_ip:9092']  # kafka broker ip
+broker = ['localhost:9092']  # kafka broker ip
 topicName = 'market'
 producer = KafkaProducer(bootstrap_servers=broker)
 
-for i, _ in enumerate(range(50)):
+count = 0
+
+for _ in range(4990):
+    count += 1
     input_data = generator_log.make_log()
     producer.send(topicName, input_data.encode())  # data ingestion
     time.sleep(0.2)
-
